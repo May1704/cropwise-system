6c2f8efc296c4e26b21d65e7aaaf8ba83def45ea
May1704 <vergara.mayleris@gmail.com>
2025-08-06 05:17:13 +0200

    start of cropwise individual final project

---
+++ src/test/java/com/cropwise/cw_system/CwSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cropwise.cw_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
